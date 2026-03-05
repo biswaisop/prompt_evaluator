c36b08785e56c9451ebd27d08612a0f34b9713f0
Biswadip mandal <95963808+biswaisop@users.noreply.github.com>
2026-03-05 21:43:41 +0530

    test

---
+++ backend/services/llm.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+from langchain_groq import ChatGroq
+
+
+client = ChatGroq(
+    api_key=os.getenv("GROQ"),
+    model_name="llama-3.3-70b-versatile",
+    temperature=0.0,
+    max_tokens=512
+)
